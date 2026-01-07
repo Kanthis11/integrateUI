110f8ea39ec5970297fb9d3a3de2cc283b8f2471
VigneshRaja102 <vignesh@salesbox.ai>
2026-01-07 14:24:29 +0530

    Added Api token

--- config.py
+++ config.py
@@ -4,10 +4,11 @@
 JENKINS_URL = "app-jenkins.salesboxai.com"   # no trailing slash
 JOB_NAME = "integrateSalesboxAI"
 USER = "kanthi"
-API_TOKEN = "115039ce30b26a9d7d943ce006885468ce"
+API_TOKEN = "1124fe272560514e18c56c3635972779e7"
 
 # Flask configuration
 FLASK_HOST = "52.52.23.133"
 FLASK_PORT = 5000
 FLASK_DEBUG = True
+
 
