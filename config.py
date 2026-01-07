13345c5bddf08bd3251d16d451844cc48e91f56e
VigneshRaja102 <vignesh@salesbox.ai>
2026-01-07 18:06:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,15 @@
 # config.py
 
 # Jenkins configuration
-JENKINS_URL = "app-jenkins.salesboxai.com"   # no trailing slash
+JENKINS_URL = "localhost:8080"   # no trailing slash
 JOB_NAME = "integrateSalesboxAI"
 USER = "kanthi"
-API_TOKEN = "1124fe272560514e18c56c3635972779e7"
+API_TOKEN = "xxxxx"
 
 # Flask configuration
-FLASK_HOST = "52.52.23.133"
+FLASK_HOST = "localhost"
 FLASK_PORT = 5000
 FLASK_DEBUG = True
+
 
 
